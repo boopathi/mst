ec255bf7e6a5754c95605b7d1bd5e131df699edb
Boopathi Rajaa <me@boopathi.in>
2019-04-06 01:32:11 +0200

    use built wasm-pack

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ yum install -y openssl-devel
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
 source ~/.cargo/env
 rustup target add wasm32-unknown-unknown
-cargo install wasm-pack --root ./
-./bin/wasm-pack build --target web
+curl -L -o wasm-pack.tar.gz 'https://github.com/rustwasm/wasm-pack/releases/download/v0.8.1/wasm-pack-v0.8.1-x86_64-unknown-linux-musl.tar.gz'
+tar xvzf wasm-pack.tar.gz
+./wasm-pack-v0.8.1-x86_64-unknown-linux-musl/wasm-pack build --target web
 mkdir dist
 cp index.html dist
 cp main.js dist
