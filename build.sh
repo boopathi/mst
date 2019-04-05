d43ee39e6c37c114d6e69d89bf7972af43872aac
Boopathi Rajaa <me@boopathi.in>
2019-04-06 01:06:54 +0200

    install to current directory

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ yum install -y openssl-devel
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
 source ~/.cargo/env
 rustup target add wasm32-unknown-unknown
-cargo install wasm-pack
-~/cargo/.bin/wasm-pack build --target web
+cargo install wasm-pack --root ./
+./bin/wasm-pack build --target web
