aa77122be48f531dccc378b29874895d55d6d168
Boopathi Rajaa <me@boopathi.in>
2019-04-06 00:56:39 +0200

    try fix now build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+set -x
+
+yum install -y openssl-devel
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
 source ~/.cargo/env
 rustup target add wasm32-unknown-unknown
 cargo install wasm-pack
-wasm-pack build --target web
+~/cargo/.bin/wasm-pack build --target web
